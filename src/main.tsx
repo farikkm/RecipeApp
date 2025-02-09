512c986592d3a4141a30a3a940e126f541899d4c
farikkm <farikk.v.2@gmail.com>
2025-02-09 19:42:58 +0500

    Warnings are corrected

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/styles/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+  <App />
 )
